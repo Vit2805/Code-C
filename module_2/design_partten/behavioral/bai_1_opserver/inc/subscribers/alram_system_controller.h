ca76f89b3910eb82c4f83e1a12bef47c30f0a6c0
Vit2805 <ngmanhtung28@gmail.com>
2025-06-02 10:41:14 +0700

    update

---
+++ module_2/design_partten/behavioral/bai_1_opserver/inc/subscribers/alram_system_controller.h
@@ -0,0 +1,10 @@
+#ifndef ALARM_SYSTEM_CONTROLLER_H
+#define ALARM_SYSTEM_CONTROLLER_H
+
+#include "../subscribers.h"
+
+typedef struct {
+    Subscriber base;
+}AlarmSystemController;
+
+#endif // ALARM_SYSTEM_CONTROLLER_H
